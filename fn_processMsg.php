ebcbb0d4d65396fcf6903855ed949138836f0e0b
Vararat <junly17@gmail.com>
2012-11-11 15:59:41 +0700

    1

--- fn_processMsg.php
+++ fn_processMsg.php
@@ -6,7 +6,7 @@
 }?>
 
 <?php function process_msg($msg,$page){
-		echo "<h2>Sorry,You can't access this page.</h2>";	
+		//echo "<h2>Sorry,You can't access this page.</h2>";	
 		echo "<h2>System is backing to home page.</h2><h2>Please wait :))</h2>";	
         echo"<meta http-equiv=\"refresh\"content=\"4;URL=home.php\">";
 		exit;
